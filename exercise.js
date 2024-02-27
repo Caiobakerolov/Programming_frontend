93f7685505646ea8ed50ef30c7ba8b277a3b40f3
Caio Bakerolov <caiobakerolov@icloud.com>
2024-02-27 22:39:26 +1000

    arranging details

---
+++ exercise.js
@@ -0,0 +1,7 @@
+"use strict";
+const multiplication = (area, height) => area * height;
+console.log(multiplication(31, 4));
+function salutation(nome) {
+    return `Hello ${nome}`;
+}
+console.log(salutation("Caio"));
